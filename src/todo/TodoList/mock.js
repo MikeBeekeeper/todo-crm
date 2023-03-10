ae4f4a12b1292a594bd2e687b006da39671d405b
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-03-10 11:25:12 +0200

    delete some objects

--- src/todo/TodoList/mock.js
+++ src/todo/TodoList/mock.js
@@ -1,62 +1,62 @@
 export const data = [
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 1,
+    id: 5478,
     name: 'Синий',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 2,
+    id: 6759,
     name: '39-й размер',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 32,
+    id: 3279,
     name: '39,5-й размер',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 65,
+    id: 6517,
     name: '40-й размер ',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 4,
+    id: 4038,
     name: '41-й размер',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 134,
+    id: 1181,
     name: '8Gb',
   },
   {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 7,
+    id: 7393,
     name: '16 Gb',
   },
-  {
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 98,
-    name: '32Gb',
-  },
-  {
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 6,
-    name: 'Space grey',
-  },
-  {
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 56,
-    name: 'Rose gold',
-  },
-  {
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 78,
-    name: 'Silver',
-  },
-  {
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
-    id: 135,
-    name: 'Silver',
-  },
+  // {
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
+  //   id: 98,
+  //   name: '32Gb',
+  // },
+  // {
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
+  //   id: 6,
+  //   name: 'Space grey',
+  // },
+  // {
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
+  //   id: 56,
+  //   name: 'Rose gold',
+  // },
+  // {
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
+  //   id: 78,
+  //   name: 'Silver',
+  // },
+  // {
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' ,
+  //   id: 135,
+  //   name: 'Silver',
+  // },
 ];
